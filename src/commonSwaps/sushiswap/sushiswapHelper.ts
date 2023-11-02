874a7fa86c263c947a980fdffbf2b80d55774e66
Jose María de la Cruz <jmariadelacruzsanchez@gmail.com>
2023-11-03 00:11:47 +0100

    fix warnings

--- src/commonSwaps/sushiswap/sushiswapHelper.ts
+++ src/commonSwaps/sushiswap/sushiswapHelper.ts
@@ -3,7 +3,7 @@ import sushiswapV2Router02Contract from "../../contracts/sushiswap/sushiswapV2Ro
 export const getSushiSwapPrice = async(tokenIn: string, tokenOut: string, amountIn: number) => {
     console.log("sushiswapV2Router02Contract", await sushiswapV2Router02Contract.methods)
     try {
-        const amountOut = await sushiswapV2Router02Contract.methods.getAmountsOut(amountIn).call();
+        // const amountOut = await sushiswapV2Router02Contract.methods.getAmountsOut(amountIn).call();
         //console.log("Amount Out", amountOut);
     } catch (error) {
         console.error("Error calling contract method:", error);
