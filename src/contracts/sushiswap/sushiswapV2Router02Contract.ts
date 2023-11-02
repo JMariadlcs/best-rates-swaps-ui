79bec379946622c505926cfd6aef8a39305a5d45
Jose María de la Cruz <jmariadelacruzsanchez@gmail.com>
2023-11-02 23:27:42 +0100

    instancciate contract using web3

--- src/contracts/sushiswap/sushiswapV2Router02Contract.ts
+++ src/contracts/sushiswap/sushiswapV2Router02Contract.ts
@@ -1,7 +1,9 @@
-import { ethers } from "ethers";
+import Web3 from 'web3'
+
 import sushiswapV2Router02ABI from './abis/sushiswapV2Router02ABI'
 
 const sushiSwapV2Router02Address = "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506" // Arbitrum Network
+const web3 = new Web3('https://arb1.arbitrum.io/rpc')
 
-export const sushiswapV2Router02Contract = new ethers.Contract(sushiSwapV2Router02Address, sushiswapV2Router02ABI);
+export const sushiswapV2Router02Contract = new web3.eth.Contract(sushiswapV2Router02ABI, sushiSwapV2Router02Address)
 
