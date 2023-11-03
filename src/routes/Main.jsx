6bd15631cd1177718928f530d79316111cffb651
Jose María de la Cruz <jmariadelacruzsanchez@gmail.com>
2023-11-03 19:35:42 +0100

    update buttons stlye

--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import '../styles/main.css'
 
 const Main = () => {
 
@@ -8,8 +9,8 @@ const Main = () => {
       <header className="App-header">
         <section className='welcome-section'>
           <h1 className='become-title'>Welcome to Best-Swaps-Rates</h1>
-          <button className="access-commonSwaps" onClick={() => navigate("/commonSwaps")}>CommonSwaps</button>
-          <button className="access-DEXAggregatorSwaps" onClick={() => navigate("/DEXAggregatorSwaps")}>DEXAggregatorSwaps</button>
+          <button className="access-button" onClick={() => navigate("/commonSwaps")}>CommonSwaps</button>
+          <button className="access-button" onClick={() => navigate("/DEXAggregatorSwaps")}>DEXAggregatorSwaps</button>
         </section>
       </header>
     </div>
