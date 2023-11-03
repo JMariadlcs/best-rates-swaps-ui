e90b4c19fc23c9986c72d7f55a03c1dbc5ebc73c
Jose María de la Cruz <jmariadelacruzsanchez@gmail.com>
2023-11-03 16:11:13 +0100

    implement sushi getAmountsOut method

--- src/commonSwaps/sushiswap/sushiswapHelper.ts
+++ src/commonSwaps/sushiswap/sushiswapHelper.ts
@@ -3,8 +3,8 @@ import sushiswapV2Router02Contract from "../../contracts/sushiswap/sushiswapV2Ro
 export const getSushiSwapPrice = async(tokenIn: string, tokenOut: string, amountIn: number) => {
     console.log("sushiswapV2Router02Contract", await sushiswapV2Router02Contract.methods)
     try {
-        // const amountOut = await sushiswapV2Router02Contract.methods.getAmountsOut(amountIn).call();
-        //console.log("Amount Out", amountOut);
+        const amountOut = await sushiswapV2Router02Contract.methods.getAmountsOut(amountIn, [tokenIn, tokenOut]).call();
+        console.log("Amount Out", amountOut);
     } catch (error) {
         console.error("Error calling contract method:", error);
     }
