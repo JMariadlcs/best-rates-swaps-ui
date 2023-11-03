b3dc472ca7e5ccd5420a04fcc7280106ed9c605d
Jose María de la Cruz <jmariadelacruzsanchez@gmail.com>
2023-11-03 17:07:08 +0100

    add return values for swap rates

--- src/commonSwaps/camelotV2/camelotV2Helper.ts
+++ src/commonSwaps/camelotV2/camelotV2Helper.ts
@@ -3,7 +3,8 @@ import camelotV2RouterContract from "../../contracts/camelot/camelotV2RouterCont
 export const getCamelotV2SwapPrice = async(tokenIn: string, tokenOut: string, amountIn: number) => {
     try {
         const amountOut = await camelotV2RouterContract.methods.getAmountsOut(amountIn, [tokenIn, tokenOut]).call();
-        console.log("CamelotV2 AmountsOut: ", amountOut);
+        console.log("CamelotV2 AmountsOut: ", amountOut[amountOut.length - 1]);
+        return amountOut[amountOut.length - 1]
     } catch (error) {
         console.error("Error calling contract method:", error);
     }
--- src/commonSwaps/sushiswap/sushiswapHelper.ts
+++ src/commonSwaps/sushiswap/sushiswapHelper.ts
@@ -3,7 +3,8 @@ import sushiswapV2Router02Contract from "../../contracts/sushiswap/sushiswapV2Ro
 export const getSushiSwapPrice = async(tokenIn: string, tokenOut: string, amountIn: number) => {
     try {
         const amountOut = await sushiswapV2Router02Contract.methods.getAmountsOut(amountIn, [tokenIn, tokenOut]).call();
-        console.log("SushiSwapV2 AmountsOut: ", amountOut);
+        console.log("SushiSwapV2 AmountsOut: ", amountOut[amountOut.length - 1]);
+        return amountOut[amountOut.length - 1]
     } catch (error) {
         console.error("Error calling contract method:", error);
     }
