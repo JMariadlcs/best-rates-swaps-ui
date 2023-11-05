39448bc446aa25a9b656ddee6e93cf7ed24b97f6
Jose María de la Cruz <jmariadelacruzsanchez@gmail.com>
2023-11-05 20:01:34 +0100

    update treasury address

--- src/contracts/addresses/contractAddresses.ts
+++ src/contracts/addresses/contractAddresses.ts
@@ -1,3 +1,3 @@
-export const commonSwapsTreasuryAddress = "0x4bd90828b5937e0757C630d1f2124096451d7AE5"
+export const commonSwapsTreasuryAddress = "0x997d3168776d9AF7A60d3664E1e69704e72F38b0"
 export const WETHAddress = "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1"
 export const USDTAddress = "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9"
