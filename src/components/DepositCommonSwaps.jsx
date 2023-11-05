fb3d1f7774118e555b1114ebda7d685e875230e7
Jose María de la Cruz <jmariadelacruzsanchez@gmail.com>
2023-11-05 17:39:01 +0100

    update approve function

--- src/components/DepositCommonSwaps.jsx
+++ src/components/DepositCommonSwaps.jsx
@@ -17,27 +17,23 @@ const DepositCommonSwaps = () => {
 
     const approveCommonSwaps = async () => {
         try {
-            console.log("web3Provider", web3Provider, txSigner);
-            const tokenContract = new web3Provider.eth.Contract(WETHAbi, WETHAddress, { from: txSigner }); // WETH
-            const approvalTx = tokenContract.methods.approve(commonSwapsTreasuryAddress, "100000000");
+            const tokenContract = new web3Provider.eth.Contract(WETHAbi, WETHAddress, { from: txSigner })
+            const approvalTx = tokenContract.methods.approve(commonSwapsTreasuryAddress, "100000000")
     
             await approvalTx.send({ from: txSigner })
             .on('transactionHash', () => {
-                // Handle the transaction hash event, e.g., update loading state.
-                setApprovalState({ ...approvalState, loading: true });
+                setApprovalState({ ...approvalState, loading: true })
             })
             .on('receipt', () => {
-                // Handle the receipt event, e.g., update done state.
-                setApprovalState({ done: true, loading: false, error: false });
+                setApprovalState({ done: true, loading: false, error: false })
             })
             .on('error', (error) => {
-                // Handle any errors, e.g., update error state.
-                console.log(`%c${error.message}`, "color: red; background: black; font-size: 20px");
+                console.log(`%c${error.message}`, "color: red; background: black; font-size: 20px")
                 setApprovalState({ ...approvalState, error: true });
-            });
+            })
         } catch (error) {
-            console.error(error);
-            setApprovalState({ ...approvalState, error: true });
+            console.error(error)
+            setApprovalState({ ...approvalState, error: true })
         }
     };
 
