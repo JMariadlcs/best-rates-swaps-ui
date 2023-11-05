8cc5355428ca293179ae39354ee4a7c25c68f9da
Jose María de la Cruz <jmariadelacruzsanchez@gmail.com>
2023-11-05 19:53:14 +0100

    update functionality

--- src/components/DepositCommonSwaps.jsx
+++ src/components/DepositCommonSwaps.jsx
@@ -1,13 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import web3 from "../contracts/web3";
 import MetaMaskConnect from "./MetaMaskConnect";
 import WETHAbi from '../contracts/abis/WETHABI.json'
 import { WETHAddress, USDTAddress, commonSwapsTreasuryAddress } from "../contracts/addresses/contractAddresses";
 import commonSwapsTreasuryABI from '../contracts/commonSwaps/abis/commonSwapsTreasuryABI.json'
 import { getUSDTBalanceInTreasury, getWETHBalanceInTreasury } from '../commonSwaps/TreasuryHelpers'
+import { getSushiSwapPrice } from '../commonSwaps/sushiswap/sushiswapHelper'
+import { getCamelotV2SwapPrice } from '../commonSwaps/camelotV2/camelotV2Helper'
 
-const DepositCommonSwaps = ( {bestProvider}) => {
+const DepositCommonSwaps = () => {
 
+    const [sushiRate, setSushiRate] = useState(undefined)
+    const [camelotRate, setCamelotRate] = useState(undefined)
+    const [bestRateProvider, setBestRateProvider] = useState(undefined)
     const [userWalletInfo, setUserWalletInfo] = useState({ address: undefined, balance: undefined });
     const [web3Provider, setWeb3Provider] = useState(undefined);
     const [txSigner, setTxSigner] = useState(undefined)
@@ -18,11 +23,32 @@ const DepositCommonSwaps = ( {bestProvider}) => {
     });
     const [WETHAmount, setWETHAmount] = useState(undefined)
     const [USDTAmount, setUSDTAmount] = useState(undefined)
+    const [amount, setAmount] = useState('')
+    const [needToloadRate, setNeedToLoadRate] = useState(true)
+    const handleDepositAmountChange = (e) => {
+        const newAmount = e.target.value
+        setAmount(e.target.value)
+    }
+
+    const getSwapRate = async () => {
+        let sushiSwapAmountOut, camelotSwapAmountOut
+        const token1 = '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1'; // WETH (Arbitrum)
+        const token2 = '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9'; // USDT (Arbitrum)
+        const amount = 1000000000000;
+
+        sushiSwapAmountOut = await getSushiSwapPrice(token1, token2, amount);
+        setSushiRate(sushiSwapAmountOut)
+        camelotSwapAmountOut = await getCamelotV2SwapPrice(token1, token2, amount);
+        setCamelotRate(camelotSwapAmountOut)
+        if (sushiSwapAmountOut >= camelotSwapAmountOut) setBestRateProvider('SushiSwap')
+        else setBestRateProvider('Camelot')
+        setNeedToLoadRate(false)
+    }
 
     const approveCommonSwaps = async () => {
         try {
             const tokenContract = new web3Provider.eth.Contract(WETHAbi, WETHAddress, { from: txSigner })
-            const approvalTx = tokenContract.methods.approve(commonSwapsTreasuryAddress, "100000000")
+            const approvalTx = tokenContract.methods.approve(commonSwapsTreasuryAddress, amount)
 
             await approvalTx.send({ from: txSigner })
                 .on('transactionHash', () => {
@@ -45,7 +71,7 @@ const DepositCommonSwaps = ( {bestProvider}) => {
     const depositWETH = async () => {
         try {
             const commonSwapsTreasuryContract = new web3Provider.eth.Contract(commonSwapsTreasuryABI, commonSwapsTreasuryAddress, { from: txSigner })
-            const depositTx = commonSwapsTreasuryContract.methods.depositWETH('100000000000000')
+            const depositTx = commonSwapsTreasuryContract.methods.depositWETH(amount)
 
             await depositTx.send({ from: txSigner })
                 .on('transactionHash', () => {
@@ -70,7 +96,7 @@ const DepositCommonSwaps = ( {bestProvider}) => {
             const WETHBalanceInTreasury = await getWETHBalanceInTreasury()
             const path = [WETHAddress, USDTAddress]
             const swapAmount = WETHBalanceInTreasury
-            const selectRouter = 1
+            const selectRouter = bestRateProvider === 'Sushiswap' ? 0 : 1
             const minAmountOut = 0
             const deadline = Date.now()
             const swapTx = await commonSwapsTreasuryContract.methods.swapWETHforUSDT(path, swapAmount.toString(), selectRouter.toString(), minAmountOut.toString(), deadline.toString())
@@ -124,7 +150,7 @@ const DepositCommonSwaps = ( {bestProvider}) => {
 
     return (
         <> {
-            !userWalletInfo.address && <p className="textdiv"> STEP 2: CONNECT METAMASK </p>
+            !userWalletInfo.address && <p className="textdiv"> STEP 1: CONNECT METAMASK </p>
         } {
 
                 !userWalletInfo.address && <MetaMaskConnect
@@ -134,21 +160,42 @@ const DepositCommonSwaps = ( {bestProvider}) => {
                 />
             }
             {
+                userWalletInfo.address && <div className='swap-rates-block'>
+                    <div className="textdiv"> STEP 2: GET SWAP RATES</div>
+                    <button onClick={getSwapRate}>Get new swap rates</button>
+                    {
+                        sushiRate && <p className="textdiv">SushiSwap WETH-USDT output: {sushiRate}</p>
+                    }
+                    {
+                        camelotRate && <p className="textdiv">CamelotSwapAmountOut WETH-USDT output: {camelotRate}</p>
+                    }
+                </div>
+            }
+            {
                 userWalletInfo.address &&
                 (<>
                     <p className="textdiv"> STEP 3: APPROVE WETH </p>
+                    <input
+                        type="number"
+                        id="amountInput"
+                        placeholder="Deposit WETH amount in WEI"
+                        value={amount}
+                        onChange={handleDepositAmountChange}
+                    />
                     <button className="ApproveCommonSwaps" onClick={approveCommonSwaps}> Approve WETH</button>
                     <p className="textdiv"> STEP 4: DEPOSIT WETH </p>
                     <button className="depositCommonSwaps" onClick={depositWETH}> Deposit WETH</button>
-                    
                 </>)
             }
             {
                 userWalletInfo.address &&
                 (<>
                     <p className="textdiv"> STEP 5: SWAP WETH TO USDT </p>
+                    {
+                        needToloadRate && <p className="needToLoadRate"> Please click the above button for getting new swap rates: {bestRateProvider} </p>
+                    }
                     <button className="reloadTreasuryBalances" onClick={swapWETHtoUSDT}> Swap whole WETH balance to USDT</button>
-                    <p className="textdiv"> Provider with better rate chosen for swapping: {bestProvider} </p>
+                    <p className="textdiv"> Provider with better rate chosen for swapping: {bestRateProvider} </p>
                 </>)
             }
             {
--- src/routes/CommonSwaps.jsx
+++ src/routes/CommonSwaps.jsx
@@ -1,31 +1,10 @@
 
-
-import { useState } from 'react';
-import { getSushiSwapPrice } from '../commonSwaps/sushiswap/sushiswapHelper'
-import { getCamelotV2SwapPrice } from '../commonSwaps/camelotV2/camelotV2Helper'
 import DepositCommonSwaps from '../components/DepositCommonSwaps'
 
 import '../styles/commonSwaps.css'
 
 const CommonSwaps = () => {
 
-    const [sushiRate, setSushiRate] = useState(undefined)
-    const [camelotRate, setCamelotRate] = useState(undefined)
-    const [bestRateProvider, setBestRateProvider] = useState(undefined)
-
-    const getSwapRate = async () => {
-        let sushiSwapAmountOut, camelotSwapAmountOut
-        const token1 = '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1'; // WETH (Arbitrum)
-        const token2 = '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9'; // USDT (Arbitrum)
-        const amount = 1000000000000;
-
-        sushiSwapAmountOut = await getSushiSwapPrice(token1, token2, amount);
-        setSushiRate(sushiSwapAmountOut)
-        camelotSwapAmountOut = await getCamelotV2SwapPrice(token1, token2, amount);
-        setCamelotRate(camelotSwapAmountOut)
-        if (sushiSwapAmountOut >= camelotSwapAmountOut) setBestRateProvider('SushiSwap')
-        else setBestRateProvider('Camelot')
-    }
 
 
 
@@ -36,17 +15,7 @@ const CommonSwaps = () => {
               <section className='commonSwaps-section'>
                 <div className="centered-container">
                   <h1 className='become-title'>Welcome to Best-Swaps-Rates</h1>
-                  <div className='swap-rates-block'>
-                    <div className="textdiv"> STEP 1: GET SWAP RATES</div>
-                    <button onClick={getSwapRate}>Get new swap rates</button>
-                    {
-                      sushiRate && <p className="textdiv">SushiSwap WETH-USDT output: {sushiRate}</p>
-                    }
-                    {
-                      camelotRate && <p className="textdiv">CamelotSwapAmountOut WETH-USDT output: {camelotRate}</p>
-                    }
-                  </div>
-                  <DepositCommonSwaps bestProvider={bestRateProvider}></DepositCommonSwaps>
+                  <DepositCommonSwaps ></DepositCommonSwaps>
                 </div>
               </section>
             </header>
