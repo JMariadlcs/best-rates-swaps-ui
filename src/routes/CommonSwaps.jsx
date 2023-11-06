a98d66d869de059adbd1d2cb75c4f71d0d2b382d
Jose María de la Cruz <jmariadelacruzsanchez@gmail.com>
2023-11-06 15:41:41 +0100

    fix stlye

--- src/routes/CommonSwaps.jsx
+++ src/routes/CommonSwaps.jsx
@@ -12,7 +12,7 @@ const CommonSwaps = () => {
               <section className='commonSwaps-section'>
                 <div className="centered-container">
                   <h1 className='become-title'>Common Swaps</h1>
-                  <p>The common swaps work in the following way: There exists a Treasury contract that permits any user to deposit WETH. All the WETH deposits are aggregated, as opposed to being segregated for each user. Subsequently, any individual can obtain the most favorable swap rate offered by SushiSwap or Camelot, and initiate a complete swap of the aggregated WETH balance to USDT. Following the swap, any user can invoke the "withdraw" function to receive the entire USDT amount from the Treasury.
+                  <p className="text-description">The common swaps work in the following way: There exists a Treasury contract that permits any user to deposit WETH. All the WETH deposits are aggregated, as opposed to being segregated for each user. Subsequently, any individual can obtain the most favorable swap rate offered by SushiSwap or Camelot, and initiate a complete swap of the aggregated WETH balance to USDT. Following the swap, any user can invoke the "withdraw" function to receive the entire USDT amount from the Treasury.
                   </p>
                   <DepositCommonSwaps ></DepositCommonSwaps>
                 </div>
